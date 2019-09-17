84c8403cea7a42f9892e4780cedb13fea991e17c
Konstantin Yegupov <kyegupov4@gmail.com>
2019-09-17 09:01:13 +0100

    fix: simplified code, reformatted using autopep8

--- birthday.py
+++ birthday.py
@@ -1,6 +1,5 @@
 from datetime import datetime, date
-startDate = date(1989,2,12)
-while startDate.year<2020:
- endDate = startDate.replace(startDate.year + 1)
- print(endDate.strftime("%A, %d. %B %Y"))
- startDate = startDate.replace(startDate.year + 1)
+startDate = date(1989, 2, 12)
+while startDate.year < 2020:
+    startDate = startDate.replace(startDate.year + 1)
+    print(startDate.strftime("%A, %d. %B %Y"))
